d66e0a9d0c3b019d3911597a8b5e6bcc9f532a04
Chris Reed <creed186@gmail.com>
2016-10-19 00:14:54 -0600

    Term dates incude -added include.js to hold term dates -added tag to load include.js in place of term dates data on every page

---
+++ include.js
@@ -0,0 +1,18 @@
+document.write('\
+<div class="tdates">\
+    <h3>Term Dates</h3>\
+    <ul>\
+<li><strong>Autumn Term 2016:</strong> Wednesday 7th September - Saturday 10th December</li>\
+<li><i>(Half Term: 22nd October - 6th November)</i></li>\
+</ul>\
+    <ul>\
+<li><strong>Spring Term 2017:</strong> Monday 9th January - Tuesday 28th April</li>\
+<li><i>(Half Term: 12th February - 26th February)</i></li>\
+</ul>\
+    <ul>\
+<strong>Easter Crash Courses 2017:</strong>\
+<li>Monday 10th - Thursday 13th April</li>\
+<li>Tuesday 18th - Friday 21st April</li>\
+</ul>\
+</div>\
+');
