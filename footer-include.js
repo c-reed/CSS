fcfcb49e6a02c79ed35cde831f88736fa322fafd
Chris Reed <creed186@gmail.com>
2019-03-21 14:37:11 -0600

    added aqualiving link and icon

---
+++ footer-include.js
@@ -0,0 +1,42 @@
+document.write('\
+<div class="container">\
+    <div class="row">\
+        <div class="col-sm-4 col-xs-12">\
+            <h4>Links</h4>\
+            <ul>\
+                <li><a href="http://www.oakwoodschool.co.uk">Oakwood Preparatory School</a></li>\
+                <li><a href="http://www.chichesterswimschool.com/hire.html">Pool Hire</a></li>\
+                <li><a href="http://www.chichesterswimschool.com/careers.html">Careers</a></li\
+                <li><a href="http://www.bigcrocodile.co.uk/">Big Crocodile</a></li>\
+                <li><a href="http://chichesterswimschool.com/pdf/CSS-Terms-and-Conditions-March-2018.pdf">Terms and Conditions</a></li>\
+            </ul>\
+            <br />\
+            <div class="sign-up" >\
+                <a href="https://goo.gl/dha5nZ" type="button" class="btn btn-default btn-lg">Sign Up Now!</a>\
+            </div>\
+        </div>\
+        <div class="col-sm-4 col-xs-12">\
+            <h4>Contact</h4>\
+            <p><span class="glyphicon glyphicon-earphone">&nbsp;</span>Tel: <a href="tel:07802880795">07802880795</a></p>\
+            <p><span class="glyphicon glyphicon-envelope">&nbsp;</span>Email: <a href="mailto:charlie@chichesterswimschool.com">charlie@chichesterswimschool.com</a></p>\
+            <address><span class="glyphicon glyphicon-home">&nbsp;</span>Address:<br />\
+            &nbsp;Oakwood Preparatory School,<br />\
+            &nbsp;Chichester,<br />\
+            &nbsp;West Sussex,<br />\
+            &nbsp;PO18 9AN<br />\
+            &nbsp;<a href="http://www.oakwoodschool.co.uk">www.oakwoodschool.co.uk</a></address>\
+        </div>\
+        <div class="affil col-sm-4 col-xs-12">\
+            <a href="https://www.sta.co.uk/"><img src="../img/STA.png" alt="STA" /></a>\
+            <a href="https://aqualiving.co.uk/"><img src="../img/aqualiving.jpg" alt="Aqualiving" /></a>\
+            <a href="http://www.bigcrocodile.co.uk/"><img src="../img/bigcroc.jpg" alt="big crococdile partner" /></a>\
+        </div>\
+    </div>\
+    <div class="row">\
+        <div class="copyr col-md-12">\
+        <p>Copyright &copy; 2015 Chichester Swim School. All Rights Reserved.</p>\
+        <p>Website by <a href="mailto:creed186@gmail.com">Chris</a>.</p>\
+        </div>\
+    </div>\
+</div>\
+');
