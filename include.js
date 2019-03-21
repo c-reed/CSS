64ad17752663f848a5f3b746c4570f60bd856454
Chris Reed <creed186@gmail.com>
2019-03-21 13:37:10 -0600

    latest updates

--- include.js
+++ include.js
@@ -2,17 +2,19 @@ document.write('\
 <div class="tdates">\
     <h3>Term Dates</h3>\
     <ul>\
-<li><strong>Autumn Term 2016:</strong> Wednesday 7th September - Saturday 10th December</li>\
-<li><i>(Half Term: 22nd October - 6th November)</i></li>\
+<li><strong>Spring Term 2019:</strong></li>\
+<li>Monday 7th January – Saturday 30th March</li>\
+<li><i>(Half Term Sunday 17th February – Sunday 24th February)</i></li>\
 </ul>\
     <ul>\
-<li><strong>Spring Term 2017:</strong> Monday 9th January - Tuesday 28th April</li>\
-<li><i>(Half Term: 12th February - 26th February)</i></li>\
+<li><strong>Easter Crash Courses:</strong></li>\
+<li>Tuesday 9th April – Friday 12th April</li>\
+<li>Tuesday 16th April – Thursday 18th April</li>\
 </ul>\
-    <ul>\
-<strong>Easter Crash Courses 2017:</strong>\
-<li>Monday 10th - Thursday 13th April</li>\
-<li>Tuesday 18th - Friday 21st April</li>\
+<ul>\
+<li><strong>Summer Term 2019:</strong></li>\
+<li>Wednesday 24th April - Tuesday 16th July</li>\
+<li><i>(Half Term Sunday 26th May - Sunday 2nd June)</i></li>\
 </ul>\
 </div>\
 ');
